1ef7703840478b5605abe2366b5d467906a4d1a3
tgrsnts <tegarsantoso72@gmail.com>
2026-05-24 14:25:27 +0700

    fix: auth login

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -6,19 +6,16 @@ use Illuminate\Foundation\Configuration\Middleware;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
-    ->withMiddleware(function (Middleware $middleware): void {
-        //
-        $middleware->redirectGuestsTo(fn () => route('auth.login'));
-    })
     ->withMiddleware(function (Middleware $middleware) {
-    $middleware->alias([
-        'is_admin' => \App\Http\Middleware\IsAdmin::class,
-    ]);
-})
+        $middleware->redirectGuestsTo(fn() => route('auth.login'));
+        $middleware->alias([
+            'is_admin' => \App\Http\Middleware\IsAdmin::class,
+        ]);
+    })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
     })->create();
